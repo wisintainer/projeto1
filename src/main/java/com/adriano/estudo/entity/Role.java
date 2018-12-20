573fb530980b4506112df083847c390f24ca0c79
Adriano Wisintainer <adriano.10152@sesc-sc.com.br>
2018-12-20 09:19:46 -0200

    first commit

---
+++ src/main/java/com/adriano/estudo/entity/Role.java
@@ -0,0 +1,27 @@
+package com.adriano.estudo.entity;
+
+import javax.persistence.*;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
